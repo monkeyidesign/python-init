43d9deb6c5fa9354513afac7befbbb6d9effdaf5
monkeyidesign <sam@monkeyidesign.com>
2021-12-14 21:56:39 +0700

    added method

---
+++ method.py
@@ -0,0 +1,27 @@
+# close() Close an open file. It has no effect if the file is already closed
+
+# detach() Seperate the underlying binary buffer from the TextIOBase and return it
+
+# fileno() Return an integer number (file descriptor) of the file.
+
+# flush() Flush the write buffer of the file stream.
+
+# isatty() Return True if the file stream is interactive
+
+# read(n) Read atmost n characters form the file. Reads till end  of file if it is negative or None
+
+# readable() Returns True if the file stream can be read from.
+
+# readline(n=-1) Read and return one line from the file. Reads in at most n bytes if specified.
+
+# seek(offset, from=SEEK_SET) Change the file position to offset bytes, in reference to from (start, current, end).
+
+# tell() Returns the current file location
+
+# truncate(size=None) Resize the file stream to size bytes. if size is not specified, resize to current location.
+
+# writeable() Returns True if the file stream can be written to
+
+# write(s) Write string s to the file and return the number of characters written
+
+# writelines(lines) Write a lines to the file.
